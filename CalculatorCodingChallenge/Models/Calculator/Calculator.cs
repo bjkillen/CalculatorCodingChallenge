5ec60adbfeee560cee73ded0a35123d5366401b4
bjkillen <blaine.j.killen@gmail.com>
2024-08-05 20:32:55 -0700

    file formatting fix

--- CalculatorCodingChallenge/Models/Calculator/Calculator.cs
+++ CalculatorCodingChallenge/Models/Calculator/Calculator.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 namespace CalculatorCodingChallenge.Models.Calculator
 {
-	public abstract class Calculator
-	{
+    public abstract class Calculator
+    {
         public Calculator()
-		{
-		}
+        {
+        }
 
-		public abstract int Calculate(int[] nums);
-	}
+        public abstract int Calculate(int[] nums);
+    }
 }
