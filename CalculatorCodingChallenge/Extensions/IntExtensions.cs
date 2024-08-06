db698636e4b09dd2748c79de5d9d00bd1d12ce35
bjkillen <blaine.j.killen@gmail.com>
2024-08-05 20:30:34 -0700

    fixed file formatting

--- CalculatorCodingChallenge/Extensions/IntExtensions.cs
+++ CalculatorCodingChallenge/Extensions/IntExtensions.cs
@@ -2,8 +2,8 @@
 
 namespace CalculatorCodingChallenge.Extensions
 {
-	public static class IntExtensions
-	{
+    public static class IntExtensions
+    {
         public static int TryParse(this string Source)
         {
             if (int.TryParse(Source, out int result))
