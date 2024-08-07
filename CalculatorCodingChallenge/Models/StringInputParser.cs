8b124882c8f5ff3c74784e447ed56fe9c9423d8a
bjkillen <blaine.j.killen@gmail.com>
2024-08-06 21:20:31 -0700

    implemented base of requirement and updated unit tests

--- CalculatorCodingChallenge/Models/StringInputParser.cs
+++ CalculatorCodingChallenge/Models/StringInputParser.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 using CalculatorCodingChallenge.Exceptions;
 using CalculatorCodingChallenge.Extensions;
@@ -11,7 +13,10 @@ namespace CalculatorCodingChallenge.Models
         {
         }
 
-        private static readonly char[] separators = new Char[] { ',', '\n' };
+        private static readonly string startingDelimiterPattern = @"^(//)(.)(\n)";
+        private static readonly Regex startingDelimiterRegex = new(StringInputParser.startingDelimiterPattern);
+
+        private static HashSet<string> separators = new() { ",", "\n" };
 
         public static int[] ParseInput(string? text)
         {
@@ -20,13 +25,38 @@ namespace CalculatorCodingChallenge.Models
                 return new int[] { 0 };
             }
 
+            // inputting \n via console received an escape while unit tests
+            // input don't. We must sanitize correctly for user input
+            string sanitizedInputText = text.Replace("\\n", "\n");
+
+            Match matchResult = startingDelimiterRegex.Match(sanitizedInputText);
+
+            if (matchResult.Success)
+            {
+                Group middleMatchingGroup = matchResult.Groups[2];
+
+                string delimeter = middleMatchingGroup.Value;
+                separators.Add(delimeter);
+
+                sanitizedInputText = Regex.Replace(
+                    sanitizedInputText,
+                    startingDelimiterPattern,
+                    ""
+                 );
+            }
+
+            return ParseInputNoDelimiter(sanitizedInputText);
+        }
+
+        private static int[] ParseInputNoDelimiter(string text)
+        {
             // TODO: Refactor this block to make one pass looking at each char,
             // parsing previous chars when delimeter found and performing
             // negative check in the same loop pass
             //
             // This will improve performance in the case requirements change
-            int[] numbers = text.Split(separators)
-                            .Select(numText => numText.Trim().TryParseWithLimit())
+            int[] numbers = text.Split(separators.ToArray(), StringSplitOptions.TrimEntries)
+                            .Select(numText => numText.TryParseWithLimit())
                             .ToArray();
 
             int[] negativeNumbers = numbers.Where(n => n < 0).ToArray();
